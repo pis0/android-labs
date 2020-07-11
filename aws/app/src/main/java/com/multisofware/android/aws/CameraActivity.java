26edc578d151a49853c89a8c4b409a74272ce182
pis0 <msdraco@gmail.com>
2020-07-11 15:03:42 -0300

    aws labs

--- aws/app/src/main/java/com/multisofware/android/aws/CameraActivity.java
+++ aws/app/src/main/java/com/multisofware/android/aws/CameraActivity.java
@@ -1,11 +1,16 @@
 package com.multisofware.android.aws;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -20,7 +25,11 @@ import com.multisofware.android.aws.camera.CameraPreview;
 import com.multisofware.android.aws.camera.IAutoFocusCallback;
 import com.multisofware.android.aws.view.BackButton;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class CameraActivity extends AppCompatActivity {
@@ -129,8 +138,9 @@ public class CameraActivity extends AppCompatActivity {
         android.hardware.Camera camera = null;
         try {
             // regular back camera
-            camera = android.hardware.Camera.open();
-            //camera = openFrontFacingCamera();
+            //camera = android.hardware.Camera.open();
+
+            camera = openFrontFacingCamera();
         } catch (Exception e) {
             Log.e(TAG, e.getMessage(), e);
         }
@@ -195,28 +205,83 @@ public class CameraActivity extends AppCompatActivity {
 
     private void processData(byte[] data) {
         try {
-            //TODO to implement
-            Log.d(TAG, "processData!!!!!! - data.length:" + data.length);
+
+            Log.d(TAG, "processData - data.length:" + data.length);
+
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeByteArray(data, 0, data.length, options);
+            final int originalW = options.outWidth;
+            final int originalH = options.outHeight;
+            float scale = (float) MainActivity.MAX_SIZE / (float) originalH;
+            options.inScaled = true;
+            options.inSampleSize = MainActivity.resolveSampleSize(options, (int) (originalW * scale), MainActivity.MAX_SIZE);
+            options.inMutable = false;
+            options.inJustDecodeBounds = false;
+
+            createImageFile(BitmapFactory.decodeByteArray(data, 0, data.length, options));
+
+
+            takePictureLock = false;
 
         } catch (Exception e) {
             Log.e(TAG, "error: " + e.getMessage(), e);
         }
     }
 
+    private void createImageFile(Bitmap original) throws IOException {
 
-    private void createImageFile(String ticketNumber, Bitmap original) throws IOException {
+        final int QUALITY = 60;
 
-        Log.d(TAG, "createImageFile - Bitmap original - " + original.getWidth() + ", " + original.getHeight());
-
-        // Save image to gallery
-        String savedImageURL = MediaStore.Images.Media.insertImage(
-                getContentResolver(),
-                original,
-                "temp",
-                "temp"
+        @SuppressLint("SimpleDateFormat")
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File dir = new File(storageDir + "/tmp");
+        if (!dir.exists()) dir.mkdir();
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                dir
         );
 
+        File file = new File(image.getAbsolutePath());
+        FileOutputStream fOut;
+        try {
+            fOut = new FileOutputStream(file);
+            original.compress(Bitmap.CompressFormat.JPEG, QUALITY, fOut);
+            fOut.flush();
+            fOut.close();
+
+            original.recycle();
+
+            Log.d(TAG, "createImageFile success - file: " + file.getAbsolutePath());
+
+            Intent returnIntent = new Intent();
+            returnIntent.putExtra("cameraData", Uri.fromFile(file).toString());
+            setResult(Activity.RESULT_OK, returnIntent);
+            finish();
+
+        } catch (Exception e) {
+            Log.e(TAG, "createImageFile error: " + e.getMessage(), e);
+        }
+
+
     }
+
+
+//    private void createImageFile(String ticketNumber, Bitmap original) throws IOException {
+//
+//        Log.d(TAG, "createImageFile - Bitmap original - " + original.getWidth() + ", " + original.getHeight());
+//
+//        // Save image to gallery
+//        String savedImageURL = MediaStore.Images.Media.insertImage(
+//                getContentResolver(),
+//                original,
+//                "temp",
+//                "temp"
+//        );
+//    }
 
 
 }
--- aws/app/src/main/java/com/multisofware/android/aws/MainActivity.java
+++ aws/app/src/main/java/com/multisofware/android/aws/MainActivity.java
@@ -2,6 +2,9 @@ package com.multisofware.android.aws;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -34,20 +37,56 @@ public class MainActivity extends AppCompatActivity {
 
     static private String TAG = MainActivity.class.getSimpleName();
 
-
     static int GALLERY_REQUEST_CODE = 1 << 0;
-    static int MAX_SIZE = 480;
+    static int CAMERA_CALLBACK = 1 << 1;
+
+    public static int MAX_SIZE = 480;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+
+        loading.dismiss();
+    }
+
+    private ProgressDialog loading;
+    private AlertDialog.Builder dialogBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        loading = new ProgressDialog(this);
+        loading.setMessage("processing...");
+        loading.setCancelable(false);
+        loading.setInverseBackgroundForced(false);
+
+        dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {
+                finish();
+            }
+        });
+        dialogBuilder.create();
+
+
         //init();
         pickFromGallery();
     }
 
-    private int resolveSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+    private void pickFromGallery() {
+
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+    }
+
+    public static int resolveSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
         final int height = options.outHeight;
         final int width = options.outWidth;
         int inSampleSize = 1;
@@ -62,6 +101,38 @@ public class MainActivity extends AppCompatActivity {
         return inSampleSize;
     }
 
+    private Image createImage(Uri imageUri) throws IOException {
+
+        ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(Objects.requireNonNull(imageUri), "r");
+        FileDescriptor fileDescriptor = Objects.requireNonNull(parcelFileDescriptor).getFileDescriptor();
+
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFileDescriptor(fileDescriptor, null, options);
+        final int originalW = options.outWidth;
+        final int originalH = options.outHeight;
+        float scale = (float) MAX_SIZE / (float) originalH;
+        options.inScaled = true;
+        options.inSampleSize = resolveSampleSize(options, (int) (originalW * scale), MAX_SIZE);
+        options.inMutable = false;
+        options.inJustDecodeBounds = false;
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor, null, options);
+        parcelFileDescriptor.close();
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        image.compress(Bitmap.CompressFormat.JPEG, 60, stream);
+        byte[] byteArray = stream.toByteArray();
+        image.recycle();
+
+        ByteBuffer sourceImageBytes = ByteBuffer.wrap(byteArray);
+
+        return new Image().withBytes(sourceImageBytes);
+    }
+
+
+    private Image source;
+    private Image target;
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -70,100 +141,136 @@ public class MainActivity extends AppCompatActivity {
             if (requestCode == GALLERY_REQUEST_CODE) {
 
                 try {
-                    Uri selectedImage = Objects.requireNonNull(data).getData();
-                    ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(Objects.requireNonNull(selectedImage), "r");
-                    FileDescriptor fileDescriptor = Objects.requireNonNull(parcelFileDescriptor).getFileDescriptor();
+                    Uri selectedImageUri = Objects.requireNonNull(data).getData();
 
-                    final BitmapFactory.Options options = new BitmapFactory.Options();
-                    options.inJustDecodeBounds = true;
-                    BitmapFactory.decodeFileDescriptor(fileDescriptor, null, options);
-                    final int originalW = options.outWidth;
-                    final int originalH = options.outHeight;
-                    float scale = (float) MAX_SIZE / (float) originalH;
-                    options.inScaled = true;
-                    options.inSampleSize = resolveSampleSize(options, (int) (originalW * scale), MAX_SIZE);
-                    options.inMutable = false;
-                    options.inJustDecodeBounds = false;
-                    Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor, null, options);
-                    parcelFileDescriptor.close();
-
-                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                    image.compress(Bitmap.CompressFormat.JPEG, 60, stream);
-                    byte[] byteArray = stream.toByteArray();
-                    image.recycle();
-
-                    ByteBuffer sourceImageBytes = ByteBuffer.wrap(byteArray);
-                    final Image source = new Image().withBytes(sourceImageBytes);
-
-                    final Image target = new Image().withBytes(sourceImageBytes);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-
-                            final AmazonRekognitionClient rekognitionClient = new AmazonRekognitionClient(new AWSCredentials() {
-                                @Override
-                                public String getAWSAccessKeyId() {
-                                    return "AKIAIPMQA5JKUG37UOSA";
-                                }
-
-                                @Override
-                                public String getAWSSecretKey() {
-                                    return "2jO9KI9FnxwD9V2YOjH8iSGJzXx/PT5s0S4dL+4f";
-                                }
-                            });
-
-                            Log.d(TAG, "rekognitionClient: " + rekognitionClient.toString());
-
-                            @SuppressLint("StaticFieldLeak")
-                            AsyncTask<String, Void, Void> task = new AsyncTask<String, Void, Void>() {
-                                @Override
-                                protected Void doInBackground(String... strings) {
-                                    Log.d(TAG, "doInBackground");
-                                    Float similarityThreshold = 90F;
-                                    CompareFacesRequest request = new CompareFacesRequest()
-                                            .withSourceImage(source)
-                                            .withTargetImage(target)
-                                            .withSimilarityThreshold(similarityThreshold);
-
-                                    Log.d(TAG, "request: " + request.toString());
-
-                                    CompareFacesResult compareFacesResult = rekognitionClient.compareFaces(request);
-                                    Log.d(TAG, "compareFacesResult: " + compareFacesResult.toString());
-                                    List<CompareFacesMatch> faceDetails = compareFacesResult.getFaceMatches();
-                                    for (CompareFacesMatch match : faceDetails) {
-                                        ComparedFace face = match.getFace();
-                                        BoundingBox position = face.getBoundingBox();
-                                        Log.d(TAG, "Face at " + position.getLeft().toString()
-                                                + " " + position.getTop()
-                                                + " matches with " + match.getSimilarity().toString()
-                                                + "% confidence.");
-                                    }
-//                                    List<ComparedFace> uncompared = compareFacesResult.getUnmatchedFaces();
-//                                    Log.d(TAG, "There was " + uncompared.size() + " face(s) that did not match");
-                                    return null;
-                                }
-                            };
-                            task.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR);
-                        }
-                    });
-
-
+                    source = createImage(selectedImageUri);
+                    if (source != null) {
+                        Log.d(TAG, "source: " + source.toString());
+                        initCamera();
+                    }
                 } catch (IOException e) {
                     Log.e(TAG, "error: " + e.getMessage(), e);
                 }
+
+            } else if (requestCode == CAMERA_CALLBACK) {
+
+                try {
+                    Uri cameraCallbackImageUri = Uri.parse(Objects.requireNonNull(data).getStringExtra("cameraData"));
+                    Log.d(TAG, "cameraCallbackImageUri: " + cameraCallbackImageUri);
+
+                    target = createImage(cameraCallbackImageUri);
+
+
+                    if (target != null) {
+                        Log.d(TAG, "target: " + target.toString());
+                        processRecognition();
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "cameraCallbackImage error:" + e.getMessage(), e);
+                }
+
+
             }
         }
     }
 
 
-    private void pickFromGallery() {
-
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        String[] mimeTypes = {"image/jpeg", "image/png"};
-        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
-
-        startActivityForResult(intent, GALLERY_REQUEST_CODE);
+    private void initCamera() {
+        Intent intent = new Intent(this, CameraActivity.class);
+        this.startActivityForResult(intent, CAMERA_CALLBACK);
     }
 
+
+    private void processRecognition() {
+
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                loading.show();
+            }
+        });
+
+
+        final AmazonRekognitionClient rekognitionClient = new AmazonRekognitionClient(new AWSCredentials() {
+            @Override
+            public String getAWSAccessKeyId() {
+                return getResources().getString(R.string.AWSAccessKeyId);
+            }
+
+            @Override
+            public String getAWSSecretKey() {
+                return getResources().getString(R.string.AWSSecretKey);
+            }
+        });
+
+        Log.d(TAG, "rekognitionClient: " + rekognitionClient.toString());
+
+        @SuppressLint("StaticFieldLeak")
+        AsyncTask<String, Void, Void> task = new AsyncTask<String, Void, Void>() {
+            @Override
+            protected Void doInBackground(String... strings) {
+
+                try {
+                    Log.d(TAG, "doInBackground");
+
+                    String faceMatchResult = "";
+
+                    Float similarityThreshold = 90F;
+                    CompareFacesRequest request = new CompareFacesRequest()
+                            .withSourceImage(source)
+                            .withTargetImage(target)
+                            .withSimilarityThreshold(similarityThreshold);
+
+                    Log.d(TAG, "request: " + request.toString());
+
+                    CompareFacesResult compareFacesResult = rekognitionClient.compareFaces(request);
+                    Log.d(TAG, "compareFacesResult: " + compareFacesResult.toString());
+                    List<CompareFacesMatch> faceDetails = compareFacesResult.getFaceMatches();
+                    for (CompareFacesMatch match : faceDetails) {
+                        ComparedFace face = match.getFace();
+                        BoundingBox position = face.getBoundingBox();
+
+                        faceMatchResult += "Face at " + position.getLeft().toString()
+                                + " " + position.getTop()
+                                + " matches with " + match.getSimilarity().toString()
+                                + "% confidence.\n\n";
+
+                        Log.d(TAG, faceMatchResult);
+
+                    }
+
+                    List<ComparedFace> uncompared = compareFacesResult.getUnmatchedFaces();
+                    faceMatchResult += "There was " + uncompared.size() + " face(s) that did not match\n\n";
+
+                    Log.d(TAG, faceMatchResult);
+
+                    dialogBuilder.setMessage(faceMatchResult);
+
+                } catch (Exception e) {
+                    Log.e(TAG, "processRecognition error:" + e.getMessage(), e);
+
+                    dialogBuilder.setMessage("fail");
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        loading.hide();
+                        dialogBuilder.show();
+                    }
+                });
+
+                return null;
+
+            }
+        };
+
+
+        task.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR);
+
+    }
+
+
 }
+
